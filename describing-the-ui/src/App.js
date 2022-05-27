185c5e1e3443f8df57ccab601cc89fce39ad18e9
X <1337816495@qq.com>
2022-05-27 17:10:22 +0800

    practice onClickCapture

--- describing-the-ui/src/App.js
+++ describing-the-ui/src/App.js
@@ -1,23 +1,45 @@
-function Profile() {
+function Floor1() {
 
     return (
-        <img
-            src = "https://i.imgur.com/MK3eW3As.jpg"
-            alt = "Katherine Johnson"
-        />
+        <div
+            className = "floor-1"
+            onClick = { _ => alert( "floor-1-bubble" ) }
+            onClickCapture = { _ => alert( "floor-1-capture" ) }
+        >
+            <Floor2 />
+        </div>
+    );
+
+}
+
+function Floor2() {
+
+    return (
+        <div
+            className = "floor-2"
+            onClick = { _ => alert( "floor-2-bubble" ) }
+            onClickCapture = { _ => alert( "floor-2-capture" ) }
+        >
+            <Floor3 />
+        </div>
+    );
+
+}
+
+function Floor3() {
+
+    return (
+        <div
+            className = "floor-3"
+            onClick = { _ => alert( "floor-3-bubble" ) }
+            onClickCapture = { _ => alert( "floor-3-capture" ) }
+        ></div>
     );
 
 }
 
 export default function App() {
 
-    return (
-        <section>
-            <h1>Amazing scientists</h1>
-            <Profile />
-            <Profile />
-            <Profile />
-        </section>
-    );
+    return <Floor1 />;
 
 }
